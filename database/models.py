1b17a28a43eb8a91b644834d48d74bf216231341
ankithkanderi24 <ankithkanderi@outlook.com>
2023-03-12 12:36:53 -0400

    Add Accounts to Database

--- database/models.py
+++ database/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
+
 class Base(DeclarativeBase):
    pass
 
@@ -55,6 +56,23 @@ class Feature(Base):
    bric_resilience: Mapped[float]
    operating_ratio: Mapped[float]
    taxes: Mapped[float]
+
+
+   def as_dict(self):
+      return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+  
+   def __init__(self, dict):
+       for key in dict:
+           setattr(self, key, dict[key])
+
+class Account(Base):
+   __tablename__ = "account"
+
+
+   username: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
+   password: Mapped[str] = mapped_column(String(50))
+   user_type: Mapped[int]
+  
 
 
    def as_dict(self):
