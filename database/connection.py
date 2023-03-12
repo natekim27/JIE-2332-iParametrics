dd7bc098308d8b0dabd674e03a53de08aa8cc5f8
ankithkanderi24 <ankithkanderi@outlook.com>
2023-03-12 12:47:08 -0400

    Create SQL Connection

---
+++ database/connection.py
@@ -0,0 +1,36 @@
+import textwrap
+import pyodbc
+import sqlalchemy as db
+import urllib
+
+
+class SQLConnectionObject:
+   def __init__(self):
+       self.driver = '{ODBC Driver 18 for SQL Server}'
+       self.server_name = 'iparametricsserver'
+       self.database_name = 'iParametrics'
+       self.server = '{server}.database.windows.net, 1433'.format(server=self.server_name)
+       self.username = "teamipara"
+       self.password = "*****"
+
+
+   def create_connection_string(self) -> str:
+       connection_string = textwrap.dedent('''
+           Driver={driver};
+           Server={server};
+           Database={database};
+           Uid={username};
+           Pwd={password};
+           Encrypt=yes;
+           TrustServerCertificate=no;
+           Connection Timeout=30;
+       '''.format(
+           driver=self.driver,
+           server=self.server,
+           database=self.database_name,
+           username=self.username,
+           password=self.password
+       ))
+       params = urllib.parse.quote_plus(connection_string)
+       connection_string = 'mssql+pyodbc:///?autocommit=true&odbc_connect={}'.format(params)
+       return connection_string
