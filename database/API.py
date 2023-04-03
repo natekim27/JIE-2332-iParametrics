31b93e189d664d74e68a6a729afe0a801dcf0022
Hemang Dash <hdash3@gatech.edu>
2023-04-03 16:06:08 -0400

    API changes and comments

--- database/API.py
+++ database/API.py
@@ -1,24 +1,34 @@
 import json
+import sys
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 from flask_restful import Resource, Api, reqparse
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from connection import get_azure_engine
 from models import Feature, Account
 from flask_cors import CORS, cross_origin
-
+from algo_dev.build_cwcs import random_forest_regression_prediction
 from compare_image import compare_floats_bar, compare_floats_pie
+
 import values
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['PROPAGATE_EXCEPTIONS'] = False
 
 engine = get_azure_engine()
 session = Session(engine)
 
+#Returns all the communities & their data [GET]
+#URL: http://127.0.0.1:5000/features/get-all
+#Request format: None
+#Response format: JSON
+#No authentication 
+#Rate limited: No 
+#Parameters - None 
 @app.route('/features/get-all', methods=['GET'])
 @cross_origin()
 def features():
@@ -27,8 +37,15 @@ def features():
    for feature in session.scalars(stmt):
        result.append(feature.as_dict())
 
-   return result, 200
+   return json.dumps(result), 200
 
+#Returns the data of a community based on the serial number
+#URL: http://127.0.0.1:5000/features/get-by-sno?sno=<serial_number>
+#Request format: None 
+#Response format: JSON
+#No authentication 
+#Rate limited: No 
+#Parameters - serial number of community 
 #Usage: /features/get-by-sno?sno=<serial_number>
 @app.route('/features/get-by-sno', methods=['GET'])
 @cross_origin()
@@ -37,6 +54,40 @@ def features_get_by_sno():
     stmt = select(Feature).where(
         Feature.serial_number.in_([serial_no])
     )
+    result = []
+
+    for feature in session.scalars(stmt):
+        result.append(feature.as_dict())
+    
+    return json.dumps(result), 200
+
+#Returns the communities within a population range
+#URL: http://127.0.0.1:5000/features/get-by-population-range?min_pop=<min_population>&max_pop=<max_population>
+#Request format: None 
+#Response format: JSON
+#No authentication 
+#Rate limited: No 
+#Parameters - minimum population, maximum population 
+#Usage: /features/get-by-population-range?min_pop=<min_population>&max_pop=<max_population>
+@app.route('/features/get-by-population-range', methods=['GET'])
+@cross_origin()
+def features_get_by_population_range():
+    min_pop = request.args.get('min_pop')
+    max_pop = request.args.get('max_pop')
+    if max_pop and min_pop:
+        stmt = select(Feature).where(
+            Feature.population >= min_pop
+        ).where(
+            Feature.population <= max_pop
+        )
+    elif min_pop:
+        stmt = select(Feature).where(
+            Feature.population >= min_pop
+        )
+    else:
+        stmt = select(Feature).where(
+            Feature.population <= max_pop
+        )
     result = []
 
     for feature in session.scalars(stmt):
@@ -115,21 +166,41 @@ def features_get_pie_chart():
 
     return send_file('./images/' + result[0]['name'] + '_' + field + '.png', mimetype='image/png'), 200
 
+#Returns whether the region has successfully been added to the database
+#URL: http://127.0.0.1:5000/features/create-region
+#Request format: JSON
+#Response format: JSON
+#No authentication 
+#Rate limited: No 
+#Parameters - None 
 @app.route('/features/create-region', methods=['POST'])
 @cross_origin()
 def features_create_region():
-    json_data = request.get_json(force=True)
-    data = json.loads(json_data)
-    if ('name' and 'namelsad' and 'stusps' in data.keys()) is False:
-        return {'Message': 'Must provide name, namelsad and stusps to add new region', 
-                'Code': 400}
+    data = request.get_json(force=True)
+    if data['name'] == None or data['namelsad'] == None or data['stusps'] == None:
+        return 'Must provide name, namelsad and stusps to add new region', 400
     
     new_region = Feature(data)
-    session.add(new_region)
-    session.commit()
+    feats = new_region.generate_cwcs_array_features()
+    cwcs = random_forest_regression_prediction(feats)
+    if cwcs:
+        new_region.cwcs = cwcs[0]
+    
+    try:
+        session.add(new_region)
+        session.commit()
+    except:
+        session.rollback()
+        raise
 
-    return {'Code': 200}
-
+    return 'Success', 200
+#Returns the region that has been deleted from the database
+#URL: http://127.0.0.1:5000/features/delete?sno=<serial_number>
+#Request format: None
+#Response format: JSON
+#No authentication 
+#Rate limited: No 
+#Parameters - serial number 
 #Usage: /features/delete?sno=<serial_number>
 @app.route('/features/delete', methods=['GET'])
 @cross_origin()
@@ -145,12 +216,19 @@ def features_delete_region():
     
     return result, 200
 
+#Returns the region that has been updated in the database, must pass in the serial number in the JSON request 
+#URL: http://127.0.0.1:5000/features/update
+#Request format: JSON
+#Response format: JSON
+#No authentication 
+#Rate limited: No 
+#Parameters - None 
+#Usage: /features/update
 #Pass in a JSON of the data parameters you want to update
 @app.route('/features/update', methods=['PUT'])
 @cross_origin()
 def features_update_region():
     data = request.get_json(force=True)
-    data = json.loads(data)
     serial_number = data['sno']
     stmt = select(Feature).where(
         Feature.serial_number.in_([serial_number])
@@ -160,7 +238,10 @@ def features_update_region():
 
     for key in data:
         setattr(feature, key, data[key])
-        
+
+    feats = feature.generate_cwcs_array_features()
+    cwcs = random_forest_regression_prediction(feats)[0]
+    setattr(feature, 'CWCS', cwcs)
     session.commit()
 
     return feature.as_dict(), 200
@@ -171,8 +252,7 @@ def authenticate():
     json_data = request.get_json(force=True)
     data = json.loads(json_data)
     if ('username' and 'password' and 'user_type' in data.keys()) is False:
-        return {'Message': 'Fill in all fields', 
-                'Code': 400}
+        return 'Fill in all fields', 400
     
     username = data['username']
     password = data['password']
@@ -188,12 +268,12 @@ def authenticate():
         account = obj
 
     if account is None:
-        return {'Code': '400'}
+        return 400
     
     if account.password == password and account.user_type == user_type:
-        return {'Code': '200'}
+        return 200
     else:
-        return {'Code': '400'}
+        return 400
     
 @app.route('/users/create-account', methods=['POST'])
 @cross_origin()
@@ -212,14 +292,11 @@ def users_create_account():
 
 
     if result:
-        return {
-            'Message': 'Username already exists',
-            'Code': '400'
-        }
+        return 'Username already exists', 400
     new_account = Account(data)
     session.add(new_account)
     session.commit()
-    return {'Code': 200}
+    return 200
 
 @app.route('/users/change-password', methods=['PUT'])
 @cross_origin()
