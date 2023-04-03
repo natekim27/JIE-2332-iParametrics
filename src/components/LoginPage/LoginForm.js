5a2c917a4a16d98f9d02e148f0a81cbffa9af4a1
Hemang Dash <hdash3@gatech.edu>
2023-04-03 15:51:11 -0400

    Login

--- src/components/LoginPage/LoginForm.js
+++ src/components/LoginPage/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 
@@ -37,6 +37,32 @@ const buttonStyle = {
 };
 
 const LoginForm = () => {
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://127.0.0.1:5000/users/authenticate`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        user_type: 0
+        }),
+    }).then(res => {
+      if (res.status === 200) {
+        setMessage("Login successful");
+        console.log(message);
+        navigate("/communitySearch", { replace: true });
+      } else {
+        console.log(res.status);
+      }
+    }).catch(error => console.error(error));
+  };
   const navigate = useNavigate();
     return (
       <div>
@@ -44,19 +70,19 @@ const LoginForm = () => {
           <h4><b>Community Willingness and Capability Score</b></h4>
         </div>
         <div style={div2Style}>
-            <Form className="LoginForm" id="loginForm">
+            <Form className="LoginForm" id="loginForm" onSubmit={handleSubmit}>
               <FormGroup>
                 <Form.Label>Username</Form.Label>
-                <FormControl type="username" placeholder="Username" autoComplete='username' />
+                <FormControl type="username" placeholder="Username" autoComplete='username' onChange={(e) => setUsername(e.target.value)}/>
                 <Form.Label>Password</Form.Label>
-                <FormControl type="password" placeholder="Password" autoComplete='password' />
+                <FormControl type="password" placeholder="Password" autoComplete='password' onChange={(e) => setPassword(e.target.value)}/>
                 <div style={buttonStyle}>
-                  <Button variant="outline-secondary" type="submit" onClick={() => navigate("/communitySearch", { replace: true })}>
+                  <Button variant="primary" type="submit">
                     Login
                   </Button>
                 </div>
                 <div style={buttonStyle}>
-                  <Button color="white" variant="outline-secondary" type="submit" onClick={() => navigate("/createAccount", { replace: true })}>
+                  <Button color="white" variant="outline-secondary" onClick={() => navigate("/createAccount", { replace: true })}>
                     Create Account
                   </Button>
                 </div>
